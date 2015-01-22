0f4145573036ed92b8fbfc50d3cf979f6ee1fcb6
greatghoul <greatghoul@gmail.com>
2015-01-22 09:19:16 +0800

    firt work version.

--- initialize.js
+++ initialize.js
@@ -1,19 +1,17 @@
 $(function() {
-  var editor, mobileToolbar, toolbar;
-
-  toolbar = ['title', 'bold', 'italic', 'underline','color', 'ol', 'ul', 'blockquote', 'code', 'link', 'hr'];
-  
-  mobileToolbar = ["bold", "underline", "strikethrough", "ul", "ol"];
-
+  var toolbar = ['title', 'bold', 'italic', 'underline','color', 'ol', 'ul', 'blockquote', 'code', 'hr'];
+  var mobileToolbar = ["bold", "underline", "strikethrough", "ul", "ol"];
   if (mobilecheck()) {
     toolbar = mobileToolbar;
   }
 
-  $('.BodyBox').each(function() {
-    new Simditor({
-      textarea: this,
-      toolbar: toolbar,
-      defaultImage: 'http://simditor.tower.im/assets/images/logo.png'
-    });
+  var editor = new Simditor({
+    textarea: $('.BodyBox'),
+    toolbar: toolbar,
+    defaultImage: 'http://simditor.tower.im/assets/images/logo.png'
+  });
+
+  $(document).on('CommentAdded', function() {
+    editor.setValue('');
   });
 });
